db337685d3e1390b1bb813aeac53960e94622016
Klaus Rechert <klaus@openslx.com>
2019-02-18 17:10:02 +0100

    make linapple work

--- src/eaas/components/impl/src/main/java/de/bwl/bwfla/emucomp/components/emulators/LinAppleBean.java
+++ src/eaas/components/impl/src/main/java/de/bwl/bwfla/emucomp/components/emulators/LinAppleBean.java
@@ -5,7 +5,9 @@ import de.bwl.bwfla.emucomp.api.Drive;
 import de.bwl.bwfla.emucomp.api.MachineConfiguration;
 import de.bwl.bwfla.emucomp.api.Nic;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Level;
 
 public class LinAppleBean extends EmulatorBean {
     @Override
@@ -27,6 +29,28 @@ public class LinAppleBean extends EmulatorBean {
 
     @Override
     protected boolean addDrive(Drive drive) {
+        if (drive == null || (drive.getData() == null)) {
+            LOG.severe("Drive doesn't contain an image, attach canceled.");
+            return false;
+        }
+
+        String unit = drive.getUnit();
+        if(unit == null || !(unit.equals("1") || unit.equals("2")))
+        {
+            LOG.severe("unit has to be either 1 or 2");
+            return false;
+        }
+
+        Path imagePath = null;
+        try {
+            imagePath = Paths.get(this.lookupResource(drive.getData(), this.getImageFormatForDriveType(drive.getType())));
+            //check if this is first image
+            emuRunner.addArguments("-" + unit, imagePath.toString());
+        } catch (Exception e) {
+            LOG.warning("Drive doesn't reference a valid binding, attach canceled." + e.getMessage());
+            LOG.log(Level.WARNING, e.getMessage(), e);
+            return false;
+        }
         return false;
     }
 
