6ca021c0fe85a143b4d7dcfae6e4d0a32ae83890
Klaus Rechert <klaus@openslx.com>
2019-02-22 11:24:26 +0100

    vmac mini

---
+++ src/eaas/components/impl/src/main/java/de/bwl/bwfla/emucomp/components/emulators/VMacMiniBean.java
@@ -0,0 +1,74 @@
+package de.bwl.bwfla.emucomp.components.emulators;
+
+import de.bwl.bwfla.common.exceptions.BWFLAException;
+import de.bwl.bwfla.emucomp.api.Drive;
+import de.bwl.bwfla.emucomp.api.MachineConfiguration;
+import de.bwl.bwfla.emucomp.api.Nic;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+
+public class VMacMiniBean extends EmulatorBean {
+    @Override
+    protected void prepareEmulatorRunner() throws BWFLAException {
+        emuRunner.setCommand("/minivmac/minivmac");
+        emuRunner.addEnvVariable("LD_LIBRARY_PATH", "/usr/local/lib");
+        emuRunner.setWorkingDirectory(Paths.get("/minivmac"));
+
+        String config = this.getNativeConfig();
+        if (config != null && !config.isEmpty()) {
+            String[] tokens = config.trim().split("\\s+");
+            for (String token : tokens)
+            {
+                if(token.isEmpty())
+                    continue;
+
+                emuRunner.addArgument(token.trim());
+            }
+        }
+    }
+
+    @Override
+    protected String getEmulatorWorkdir()
+    {
+        return "/minivmac";
+    }
+
+    @Override
+    protected String getEmuContainerName(MachineConfiguration env)
+    {
+        return "vmacmini";
+    }
+
+    @Override
+    protected boolean addDrive(Drive drive) {
+        if (drive == null || (drive.getData() == null)) {
+            LOG.severe("Drive doesn't contain an image, attach canceled.");
+            return false;
+        }
+        Path imagePath = null;
+        try {
+            imagePath = Paths.get(this.lookupResource(drive.getData(), this.getImageFormatForDriveType(drive.getType())));
+
+            emuRunner.addArguments(imagePath.toString());
+            return true;
+        } catch (Exception e) {
+            LOG.warning("Drive doesn't reference a valid binding, attach canceled." + e.getMessage());
+            LOG.log(Level.WARNING, e.getMessage(), e);
+            return false;
+        }
+    }
+
+    @Override
+    protected boolean connectDrive(Drive drive, boolean attach) {
+        return false;
+    }
+
+    @Override
+    protected boolean addNic(Nic nic) {
+        return false;
+    }
+}
+
