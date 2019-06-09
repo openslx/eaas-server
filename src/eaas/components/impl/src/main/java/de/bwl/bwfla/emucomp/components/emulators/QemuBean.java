2b9f18812f45b7828b27d79f1f5e3996bcfe79a8
Klaus Rechert <klaus@openslx.com>
2019-06-09 17:34:45 +0200

    qemu hotfix

--- src/eaas/components/impl/src/main/java/de/bwl/bwfla/emucomp/components/emulators/QemuBean.java
+++ src/eaas/components/impl/src/main/java/de/bwl/bwfla/emucomp/components/emulators/QemuBean.java
@@ -75,8 +75,8 @@ public class QemuBean extends EmulatorBean
 			qemu_bin += emuEnvironment.getArch();
 
 		File exec = new File(qemu_bin);
-		if (exec == null || !exec.exists())
-			throw new BWFLAException("EmulatorContainer's executable not found! Make sure you have specified " + "a valid path to your executable in the corresponding 'properties' file");
+		//if (exec == null || !exec.exists())
+		//	throw new BWFLAException("EmulatorContainer's executable not found! Make sure you have specified " + "a valid path to your executable in the corresponding 'properties' file");
 
 		String config = this.getNativeConfig();
 		// Initialize the process-runner
