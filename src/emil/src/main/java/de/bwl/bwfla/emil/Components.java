90ddf2c90ec51073dcfb61bac0d008067eecaef2
Klaus Rechert <klaus@openslx.com>
2019-06-22 18:12:28 +0200

    remove debug output

--- src/emil/src/main/java/de/bwl/bwfla/emil/Components.java
+++ src/emil/src/main/java/de/bwl/bwfla/emil/Components.java
@@ -1406,7 +1406,6 @@ public class Components {
             for(EventObserver eo : observerList) {
                 eventTask = scheduler.scheduleAtFixedRate(
                 () -> {
-                    System.out.println(eo.getName());
                     for(String m : eo.messages()) {
                         sink.send(sse.newEventBuilder().name(eo.getName())
                                 .data(String.class, m).build());
