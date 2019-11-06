openslx/eaas-server

commit f929ab6f73bf27a9faec4e18c28a948179bda648

make webRTC an UI option

src/eaas/components/impl/src/main/java/de/bwl/bwfla/emucomp/components/emulators/EmulatorBean.java
@@ -110,8 +110,6 @@ public abstract class EmulatorBean extends EaasComponentBean implements Emulator
 	@Config("components.emulator_containers.snapshot")
 	public boolean isSnapshotEnabled = false;
 
-	@Inject
-	@Config("emucomp.enable_pulseaudio")
 	private boolean isPulseAudioEnabled = false;
 
 	@Inject
@@ -1435,11 +1433,17 @@ public abstract class EmulatorBean extends EaasComponentBean implements Emulator
 						this.setEmulatorTime(offset);
 					}
 				}
+
+				this.isPulseAudioEnabled = false;
+				if(uiOptions.getAudio_system() != null
+						&& !uiOptions.getAudio_system().isEmpty()
+						&& uiOptions.getAudio_system().equalsIgnoreCase("webrtc")) {
+					this.isPulseAudioEnabled = true;
+				}
 			}
 
 			this.setupEmulatorBackend();
-
-
+			
 			for(Drive drive: emuEnvironment.getDrive())
 				prepareDrive(drive);
 

src/eaas/components/api/src/main/java/de/bwl/bwfla/emucomp/api/UiOptions.java
@@ -26,12 +26,7 @@ import javax.xml.bind.annotation.XmlType;
 
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "uiOptions", namespace = "http://bwfla.bwl.de/common/datatypes", propOrder = {
-    "html5",
-    "input",
-    "time",
-	"forwarding_system"
-})
+@XmlType(name = "uiOptions", namespace = "http://bwfla.bwl.de/common/datatypes")
 public class UiOptions {
 
     @XmlElement(namespace = "http://bwfla.bwl.de/common/datatypes")
@@ -45,6 +40,9 @@ public class UiOptions {
 
 	@XmlElement(namespace = "http://bwfla.bwl.de/common/datatypes")
 	private String forwarding_system;
+
+	@XmlElement(namespace = "http://bwfla.bwl.de/common/datatypes")
+	private String audio_system;
 
 	public String getForwarding_system() {
 		return forwarding_system;
@@ -76,5 +74,13 @@ public class UiOptions {
 
 	public void setTime(TimeOptions time) {
 		this.time = time;
+	}
+
+	public String getAudio_system() {
+		return audio_system;
+	}
+
+	public void setAudio_system(String audio_system) {
+		this.audio_system = audio_system;
 	}
 }

src/emil/src/main/java/de/bwl/bwfla/emil/EmilEnvironmentData.java
@@ -474,6 +474,14 @@ public class EmilEnvironmentData extends EmilRest {
 				else
 					machineConfiguration.getUiOptions().setForwarding_system(null);
 
+				if(desc.isUseWebRTC())
+				{
+					LOG.severe("webRTC");
+					machineConfiguration.getUiOptions().setAudio_system("webRTC");
+				}
+				else
+					machineConfiguration.getUiOptions().setForwarding_system(null);
+
 				if (machineConfiguration.getUiOptions().getHtml5() == null)
 					machineConfiguration.getUiOptions().setHtml5(new Html5Options());
 

src/emil/src/main/java/de/bwl/bwfla/emil/datatypes/rest/EnvironmentDetails.java
@@ -85,8 +85,11 @@ public class EnvironmentDetails {
     @XmlElement
     private String nativeConfig;
 
-    @XmlElement
-    private boolean useXpra;
+    @XmlElement(defaultValue = "false")
+    private boolean useXpra = false;
+
+    @XmlElement(defaultValue = "false")
+    private boolean useWebRTC = false;
 
     @XmlElement private String containerName;
     @XmlElement private String containerVersion;
@@ -193,13 +196,16 @@ public class EnvironmentDetails {
             if(machineConf.getNativeConfig() != null)
                 this.nativeConfig = machineConf.getNativeConfig().getValue();
 
-            if(machineConf.getUiOptions() != null && machineConf.getUiOptions().getForwarding_system() != null)
+            if(machineConf.getUiOptions() != null )
             {
-                if (machineConf.getUiOptions().getForwarding_system().equalsIgnoreCase("xpra"))
+                if (machineConf.getUiOptions().getForwarding_system() != null &&
+                        machineConf.getUiOptions().getForwarding_system().equalsIgnoreCase("xpra"))
                     this.useXpra = true;
+
+                if (machineConf.getUiOptions().getAudio_system() != null &&
+                        machineConf.getUiOptions().getAudio_system().equalsIgnoreCase("webRTC"))
+                    this.useWebRTC = true;
             }
-            else
-                this.useXpra = false;
 
             if (machineConf.getEmulator().getBean() != null) {
                 this.emulator = machineConf.getEmulator().getBean();

src/emil/src/main/java/de/bwl/bwfla/emil/datatypes/rest/UpdateEnvironmentDescriptionRequest.java
@@ -31,6 +31,7 @@ public class UpdateEnvironmentDescriptionRequest extends EmilRequestType
 	private boolean enableRelativeMouse;
 	private boolean shutdownByOs;
 	private boolean useXpra;
+	private boolean useWebRTC;
 	private String xpraEncoding;
 	private boolean canProcessAdditionalFiles;
 	private List<Drive> drives;
@@ -173,5 +174,13 @@ public class UpdateEnvironmentDescriptionRequest extends EmilRequestType
 
 	public void setDrives(List<Drive> drives) {
 		this.drives = drives;
+	}
+
+	public boolean isUseWebRTC() {
+		return useWebRTC;
+	}
+
+	public void setUseWebRTC(boolean useWebRTC) {
+		this.useWebRTC = useWebRTC;
 	}
 }

commit 30184801ef1807952180b091734aba98a9e4f77a

fix PulseAudio-related initialization bug introduced in f929ab6f

src/eaas/components/impl/src/main/java/de/bwl/bwfla/emucomp/components/emulators/EmulatorBean.java
@@ -1419,7 +1419,6 @@ public abstract class EmulatorBean extends EaasComponentBean implements Emulator
 
 			MachineConfiguration.NativeConfig nativeConfig = emuEnvironment.getNativeConfig();
 			this.prepareNativeConfig(nativeConfig);
-			this.prepareEmulatorRunner();
 
 			UiOptions uiOptions = emuEnvironment.getUiOptions();
 			if (uiOptions != null) {
@@ -1442,6 +1441,7 @@ public abstract class EmulatorBean extends EaasComponentBean implements Emulator
 				}
 			}
 
+			this.prepareEmulatorRunner();
 			this.setupEmulatorBackend();
 			
 			for(Drive drive: emuEnvironment.getDrive())
@@ -1594,6 +1594,12 @@ public abstract class EmulatorBean extends EaasComponentBean implements Emulator
 		emuRunner.addEnvVariable("SDL_SRVCTLSOCKET", ctlSocket.getName());
 		emuRunner.addEnvVariable("SDL_EMUCTLSOCKET", emuCtlSocketName);
 		emuConfig.setIoSocket(emusocket);
+
+		// HACK: Qemu uses a custom audio setup!
+		if (this instanceof QemuBean) {
+			emuRunner.getEnvVariables()
+					.remove("SDL_AUDIODRIVER");
+		}
 
 		emuConfig.setInactivityTimeout(this.getInactivityTimeout());
 