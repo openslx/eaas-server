55d0ec36c29866cffcac1df3cc5319859c280159
Klaus Rechert <klaus@openslx.com>
2020-02-24 17:08:55 +0100

    RSA / JWK

--- src/emil/src/main/java/de/bwl/bwfla/emil/filters/AuthenticationFilter.java
+++ src/emil/src/main/java/de/bwl/bwfla/emil/filters/AuthenticationFilter.java
@@ -13,8 +13,12 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.net.URL;
+import java.security.interfaces.RSAPublicKey;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
+import com.auth0.jwk.*;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -44,10 +48,16 @@ public class AuthenticationFilter implements ContainerRequestFilter {
     private String authAudience;
 
     @Inject
+    @Config(value = "emil.authJwksUri")
+    private String authJwksUri;
+
+    @Inject
     @AuthenticatedUser
     Event<JwtLoginEvent> userAuthenticatedEvent;
 
     protected static final Logger LOG = Logger.getLogger("Authentication");
+
+    private static JwkProvider provider = null;
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
@@ -70,6 +80,14 @@ public class AuthenticationFilter implements ContainerRequestFilter {
             token = map.getFirst("access_token");
         }
         // String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCJ9.AbIJTDMFc7yUa5MhvcP03nJPyCPzZtQcGEp-zWfOkEE";
+
+
+        if(provider == null && authJwksUri != null) {
+            JwkProvider provider = new JwkProviderBuilder(new URL(authJwksUri))
+                    .cached(10, 24, TimeUnit.HOURS)
+                    .rateLimited(10, 1, TimeUnit.MINUTES)
+                    .build();
+        }
 
         if(token == null) {
             LOG.warning("anonymous");
@@ -96,10 +114,14 @@ public class AuthenticationFilter implements ContainerRequestFilter {
 
         try {
             // System.out.println(" secret " + authSecret);
-            Algorithm algorithm = Algorithm.HMAC256(authSecret);
+            DecodedJWT jwt = JWT.decode(token);
+            String keyId = jwt.getKeyId();
+            Jwk jwk = provider.get(keyId);
+
+            Algorithm algorithm = Algorithm.RSA256((RSAPublicKey)jwk);
             JWTVerifier verifier = JWT.require(algorithm)
                     .build(); //Reusable verifier instance
-            DecodedJWT jwt = verifier.verify(token);
+            jwt = verifier.verify(token);
             if(authAudience != null && jwt.getClaim("aud") != null)
             {
                 if(!jwt.getClaim("aud").equals(authAudience))
@@ -107,10 +129,10 @@ public class AuthenticationFilter implements ContainerRequestFilter {
                     userAuthenticatedEvent.fire(new JwtLoginEvent(null));
                     throw new JWTVerificationException("audience mismatch");
                 }
-
             }
             userAuthenticatedEvent.fire(new JwtLoginEvent(jwt));
-        } catch (JWTVerificationException exception){
+        } catch (JWTVerificationException | JwkException exception){
+            exception.printStackTrace();
             throw exception;
         }
     }
