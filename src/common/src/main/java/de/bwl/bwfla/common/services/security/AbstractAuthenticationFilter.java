6d54a4d48c49f7ddba322ebec22d6b506493ba3d
Klaus Rechert <klaus@openslx.com>
2020-06-04 12:09:12 +0200

    reduce noize

--- src/common/src/main/java/de/bwl/bwfla/common/services/security/AbstractAuthenticationFilter.java
+++ src/common/src/main/java/de/bwl/bwfla/common/services/security/AbstractAuthenticationFilter.java
@@ -91,7 +91,7 @@ public abstract class AbstractAuthenticationFilter implements ContainerRequestFi
         final DecodedJWT jwt = JWT.decode(token);
         final String keyId = jwt.getKeyId();
         final Jwk jwk = provider.get(keyId);
-        LOG.severe(jwk.getPublicKey().getAlgorithm() + " " + jwk.getPublicKey().toString());
+        // LOG.severe(jwk.getPublicKey().getAlgorithm() + " " + jwk.getPublicKey().toString());
         return this.verify(token, Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey()));
     }
 
