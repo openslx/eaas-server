4f020df7cd9a302be1178b42dec8e837929fbbec
Klaus Rechert <klaus@openslx.com>
2021-05-06 11:24:08 +0200

    moved code to static utils, to allow usage in internal tasks

--- src/emil/src/main/java/de/bwl/bwfla/emil/tasks/ImportEmulatorTask.java
+++ src/emil/src/main/java/de/bwl/bwfla/emil/tasks/ImportEmulatorTask.java
@@ -7,6 +7,7 @@ import de.bwl.bwfla.common.taskmanager.CompletableTask;
 import de.bwl.bwfla.emil.DatabaseEnvironmentsAdapter;
 import de.bwl.bwfla.emil.datatypes.rest.CreateContainerImageResult;
 import de.bwl.bwfla.emil.datatypes.rest.ImportEmulatorRequest;
+import de.bwl.bwfla.emil.utils.ImportEmulatorUtil;
 import de.bwl.bwfla.emucomp.api.FileSystemType;
 import de.bwl.bwfla.emucomp.api.ImageArchiveBinding;
 import de.bwl.bwfla.imagearchive.util.EnvironmentsAdapter;
@@ -19,7 +20,6 @@ import java.util.concurrent.CompletableFuture;
 
 public class ImportEmulatorTask extends BlockingTask<Object> {
 
-    private static final String EMULATOR_DEFAULT_ARCHIVE = "emulators";
     private final DatabaseEnvironmentsAdapter envHelper;
     private final ImportEmulatorRequest emulatorRequest;
 
@@ -29,71 +29,9 @@ public class ImportEmulatorTask extends BlockingTask<Object> {
         this.emulatorRequest = emulatorRequest;
     }
 
-    private String getEmulatorArchive() {
-        String archive = ConfigurationProvider.getConfiguration().get("emucomp.emulator_archive");
-        if(archive == null || archive.isEmpty())
-            return EMULATOR_DEFAULT_ARCHIVE;
-        return archive;
-    }
-
-    private void importEmulator(ImportEmulatorRequest emulatorRequest) throws BWFLAException, MalformedURLException {
-        ImageArchiveMetadata meta = new ImageArchiveMetadata();
-        meta.setType(ImageType.BASE);
-
-        EnvironmentsAdapter.ImportImageHandle importState = null;
-        ImageArchiveBinding binding;
-        importState = envHelper.importImage(getEmulatorArchive(), new URL(emulatorRequest.getImageUrl()), meta, true);
-        binding = importState.getBinding(60 * 60 * 60); //wait an hour
-
-        de.bwl.bwfla.api.imagearchive.ImageDescription iD = new de.bwl.bwfla.api.imagearchive.ImageDescription();
-        EmulatorMetadata emulatorMetadata = envHelper.extractMetadata(binding.getImageId());
-        iD.setType(meta.getType().value());
-        iD.setId(binding.getImageId());
-        iD.setFstype(FileSystemType.EXT4.toString());
-
-        ImageMetadata entry = new ImageMetadata();
-        Alias alias = new Alias();
-
-        entry.setImage(iD);
-        Provenance pMd = new Provenance();
-
-        if (emulatorMetadata != null) {
-            pMd.setOciSourceUrl(emulatorMetadata.getOciSourceUrl());
-            pMd.setVersionTag(emulatorMetadata.getEmulatorVersion());
-            entry.setProvenance(pMd);
-
-            entry.setName("emucon-rootfs/" + emulatorMetadata.getEmulatorType());
-            entry.setVersion(emulatorMetadata.getEmulatorVersion());
-            entry.setDigest(emulatorMetadata.getContainerDigest());
-
-            alias.setName("emucon-rootfs/" + emulatorMetadata.getEmulatorType());
-            alias.setVersion(emulatorMetadata.getEmulatorVersion());
-            alias.setAlias(emulatorMetadata.getEmulatorVersion());
-        } else if (emulatorRequest.getMetadata() != null) {
-            CreateContainerImageResult.ContainerImageMetadata cmd = emulatorRequest.getMetadata();
-            pMd.setOciSourceUrl(cmd.getContainerSourceUrl());
-            pMd.setVersionTag(cmd.getTag());
-            entry.setProvenance(pMd);
-
-            if( cmd.getEmulatorType() == null || cmd.getEmulatorVersion() == null)
-                throw new BWFLAException("not a emulator container or unsupported metadata format");
-
-            entry.setName("emucon-rootfs/" + cmd.getEmulatorType());
-            entry.setVersion(cmd.getEmulatorVersion());
-            entry.setDigest(cmd.getContainerDigest());
-
-            alias.setName("emucon-rootfs/" + cmd.getEmulatorType());
-            alias.setVersion(cmd.getEmulatorVersion());
-            alias.setAlias(cmd.getEmulatorVersion());
-        } else {
-            throw new BWFLAException("not a emulator container or unsupported metadata format");
-        }
-        envHelper.addNameIndexesEntry(getEmulatorArchive(), entry, alias);
-    }
-
     @Override
     protected Object execute() throws Exception {
-        importEmulator(emulatorRequest);
+        ImportEmulatorUtil.doImport(emulatorRequest, envHelper);
         return null;
     }
 }
