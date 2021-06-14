833a06fd2171c4bb1b0b7939c3e9a2c09b80e905
Klaus Rechert <klaus@openslx.com>
2021-06-14 21:24:33 +0200

    use crane instead of skopeo

--- src/imagebuilder/impl/src/main/java/de/bwl/bwfla/imagebuilder/DockerTools.java
+++ src/imagebuilder/impl/src/main/java/de/bwl/bwfla/imagebuilder/DockerTools.java
@@ -4,9 +4,12 @@ import de.bwl.bwfla.common.exceptions.BWFLAException;
 import de.bwl.bwfla.common.utils.DeprecatedProcessRunner;
 import de.bwl.bwfla.imagebuilder.api.ImageContentDescription;
 
+import javax.json.*;
 import java.io.IOException;
+import java.io.StringReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,32 +17,21 @@ import java.util.logging.Logger;
 class DockerTools {
 
     private final Logger log;
-    private Path workdir;
     private ImageContentDescription.DockerDataSource ds;
 
     DockerTools(Path workdir, ImageContentDescription.DockerDataSource dockerDataSource, Logger log) throws BWFLAException {
         this.log = log;
-        this.workdir = workdir;
         this.ds = dockerDataSource;
 
         if (ds.imageRef == null)
             throw new BWFLAException("Docker image ref and/or tag must not be null");
     }
 
-    public void pull() throws BWFLAException {
-        ds.dockerDir = workdir.resolve("image");
-        try {
-            Files.createDirectories(ds.dockerDir);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new BWFLAException(e);
-        }
 
+    public void pull(Path destDir) throws BWFLAException {
+        
         if(ds.imageRef == null)
             throw new BWFLAException("image ref is not set");
-
-        if(!ds.imageRef.startsWith("docker://")) // assume docker-style registry for now
-            ds.imageRef = "docker://" + ds.imageRef;
 
         if(ds.digest == null || ds.digest.isEmpty())
             ds.digest = getDigest();
@@ -49,78 +41,115 @@ class DockerTools {
 
         log.info("Copying digest " + ds.digest + "...");
 
-        DeprecatedProcessRunner skopeoRunner = new DeprecatedProcessRunner();
-        skopeoRunner.setCommand("skopeo");
-        skopeoRunner.addArgument("--insecure-policy");
-        skopeoRunner.addArgument("copy");
-        skopeoRunner.addArgument( ds.imageRef + "@" + ds.digest);
-        skopeoRunner.addArgument("oci:" + ds.dockerDir);
-        skopeoRunner.setLogger(log);
-        if (!skopeoRunner.execute()) {
-            try {
-                Files.deleteIfExists(ds.dockerDir);
-            } catch (IOException e) {
-                log.log(Level.WARNING, "Running skopeo failed!", e);
-            }
-            ds.dockerDir = null;
-            throw new BWFLAException("Skopeo failed to fetch image from DockerHub");
-        }
+        DeprecatedProcessRunner runner_ = new DeprecatedProcessRunner();
+        runner_.setLogger(log);
+        runner_.setCommand("/bin/bash");
+        runner_.setWorkingDirectory(destDir);
+        runner_.addArgument("-c");
+        runner_.addArgument("crane export " + ds.imageRef + "@" + ds.digest + " - | tar x");
+        if(!runner_.execute(true))
+            throw new BWFLAException("crane export failed.");
 
-        ds.emulatorType = getLabel(".Labels.EAAS_EMULATOR_TYPE");
+        String config = getConfig();
+
+        ds.emulatorType = jq(config, ".Labels.EAAS_EMULATOR_TYPE");
         log.info("Emulator's type: " + ds.emulatorType);
 
-        ds.version = getLabel(".Labels.EAAS_EMULATOR_VERSION");
+        ds.version = jq(config, ".Labels.EAAS_EMULATOR_VERSION");
         log.info("Emulator's version: " + ds.version);
-    }
 
-    public void unpack() throws BWFLAException {
-        if (ds.dockerDir == null || !Files.exists(ds.dockerDir))
-            throw new BWFLAException("Cannot unpack docker! pull() first");
+        String md = jq(config, "{Cmd: .config.Cmd, Env: .config.Env, WorkingDir: .config.WorkingDir}");
+        if(md == null)
+        {
+            log.warning("Docker metadata seems to be incomplete. Could not determine, CMD, ENV or WORKINGDIR. \n" + config);
+            return;
+        }
+
+        final JsonReader reader = Json.createReader(new StringReader(md));
+
+        final JsonObject json = reader.readObject();
+        final ArrayList<String> envvars = new ArrayList<>();
 
         try {
-            ds.rootfs = Files.createDirectory(workdir.resolve("rootfs"));
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new BWFLAException(e);
+            JsonArray envArray = json.getJsonArray("Env");
+            for (int i = 0; i < envArray.size(); i++)
+                envvars.add(envArray.getString(i));
+        }
+        catch(ClassCastException e)
+        {
+            log.warning("importing ENV failed");
+            log.warning("Metadata object " + json.toString());
         }
 
-        DeprecatedProcessRunner ociRunner = new DeprecatedProcessRunner();
-        ociRunner.setCommand("oci-image-tool");
-        ociRunner.addArgument("unpack");
-        ociRunner.addArguments("--ref", "name=");
-        ociRunner.addArgument(ds.dockerDir.toString());
-        ociRunner.addArgument(ds.rootfs.toString());
-        ociRunner.setLogger(log);
-        if (!ociRunner.execute()) {
-            try {
-                Files.deleteIfExists(ds.rootfs);
-            } catch (IOException e) {
-                log.log(Level.WARNING, "Running oci-image-tool failed!", e);
-            }
-            ds.rootfs = null;
-            throw new BWFLAException("Skopeo failed to unpack image from DockerHub");
+        final ArrayList<String> cmds = new ArrayList<>();
+        try {
+            JsonArray cmdJson = json.getJsonArray("Cmd");
+            for (int i = 0; i < cmdJson.size(); i++)
+                cmds.add(cmdJson.getString(i));
         }
-        ds.layers = getLayer();
+        catch (ClassCastException e)
+        {
+            log.warning("importing CMD failed");
+            log.warning("Metadata object " + json.toString());
+        }
+
+        try {
+            JsonString workDirObject = json.getJsonString("WorkingDir");
+            if (workDirObject != null) {
+                ds.workingDir = workDirObject.getString();
+            }
+        }
+        catch(ClassCastException e)
+        {
+            log.warning("importing WorkingDir failed");
+            log.warning("Metadata object " + json.toString());
+        }
+        ds.entryProcesses = cmds;
+        ds.envVariables = envvars;
+
     }
 
-    private String getLabel(String l) throws BWFLAException
+    private String getConfig() throws BWFLAException
     {
         DeprecatedProcessRunner runner = new DeprecatedProcessRunner();
-        runner.setCommand("/bin/bash");
-        runner.addArguments("-c", "skopeo inspect oci://" + ds.dockerDir.toString() + "| jq -r " + l);
+        runner.setCommand("crane");
+        runner.addArguments("config", ds.imageRef + "@" + ds.digest);
         runner.setLogger(log);
+
         try {
             final DeprecatedProcessRunner.Result result = runner.executeWithResult(false)
                     .orElse(null);
 
             if (result == null || !result.successful())
-                throw new BWFLAException("Running skopeo failed!");
+                throw new BWFLAException("Running crane failed!");
 
             return result.stdout()
                     .trim();
         }
         catch (IOException error) {
-            throw new BWFLAException("Parsing docker label failed!", error);
+            throw new BWFLAException("Fetching docker config failed", error);
+        }
+    }
+
+    private String jq(String config, String query) throws BWFLAException
+    {
+        DeprecatedProcessRunner runner = new DeprecatedProcessRunner();
+        runner.setCommand("/bin/bash");
+        runner.addEnvVariable("config", config);
+        runner.addArguments("-c", "echo \"$config\" | jq -r \"" + query + "\"");
+        runner.setLogger(log);
+        try {
+            final DeprecatedProcessRunner.Result result = runner.executeWithResult(true)
+                    .orElse(null);
+
+            if (result == null || !result.successful())
+                return null;
+
+            return result.stdout()
+                    .trim();
+        }
+        catch (IOException error) {
+            throw new BWFLAException("Parsing docker config failed!", error);
         }
     }
 
@@ -131,41 +160,21 @@ class DockerTools {
             ds.tag = "latest";
 
         DeprecatedProcessRunner runner = new DeprecatedProcessRunner();
-        runner.setCommand("/bin/bash");
-        runner.addArguments("-c", "skopeo inspect \"$1\":\"$2\" | jq -r .Digest", "", ds.imageRef, ds.tag);
         runner.setLogger(log);
-        try {
+        runner.setCommand("crane");
+        runner.addArgument("digest");
+        runner.addArgument(ds.imageRef + ":" + ds.tag);
+          try {
             final DeprecatedProcessRunner.Result result = runner.executeWithResult(true)
                     .orElse(null);
-
             if (result == null || !result.successful())
-                throw new BWFLAException("Running skopeo failed!");
+                throw new BWFLAException("Running crane failed!");
 
             return result.stdout()
                     .trim();
         }
         catch (IOException error) {
             throw new BWFLAException("Parsing docker digest failed!", error);
-        }
-    }
-
-    private String[] getLayer() throws  BWFLAException {
-        DeprecatedProcessRunner runner = new DeprecatedProcessRunner();
-        runner.setCommand("oci-layer-list.sh");
-        runner.addArgument(ds.dockerDir.toString());
-        runner.setLogger(log);
-        try {
-            final DeprecatedProcessRunner.Result result = runner.executeWithResult(false)
-                    .orElse(null);
-
-            if (result == null || !result.successful())
-                throw new BWFLAException("Running oci-layer-list.sh failed!");
-
-            return result.stdout()
-                    .split("\\r?\\n");
-        }
-        catch (IOException error) {
-            throw new BWFLAException("Parsing docker layers failed!", error);
         }
     }
 }
