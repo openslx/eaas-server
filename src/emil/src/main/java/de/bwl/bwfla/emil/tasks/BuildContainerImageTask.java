96752c3bdc0c96fa233f212ead61ff7745994b99
Jurek <jurek.oberhauser@gmx.net>
2021-12-17 14:41:01 +0100

    cleanup imports + other review improvements

--- src/emil/src/main/java/de/bwl/bwfla/emil/tasks/BuildContainerImageTask.java
+++ src/emil/src/main/java/de/bwl/bwfla/emil/tasks/BuildContainerImageTask.java
@@ -1,30 +1,18 @@
 package de.bwl.bwfla.emil.tasks;
 
 import com.openslx.eaas.imagearchive.ImageArchiveClient;
-import de.bwl.bwfla.api.imagearchive.ImageMetadata;
-import de.bwl.bwfla.api.imagebuilder.DockerImport;
-import de.bwl.bwfla.api.imagebuilder.ImageBuilder;
-import de.bwl.bwfla.api.imagebuilder.ImageBuilderResult;
+
 import de.bwl.bwfla.common.exceptions.BWFLAException;
 import de.bwl.bwfla.common.taskmanager.BlockingTask;
 import de.bwl.bwfla.common.utils.DeprecatedProcessRunner;
 import de.bwl.bwfla.emil.EmilEnvironmentRepository;
 import de.bwl.bwfla.emil.datatypes.rest.ComponentResponse;
 import de.bwl.bwfla.emil.datatypes.rest.CreateContainerImageRequest;
-import de.bwl.bwfla.emil.datatypes.rest.CreateContainerImageResult;
 import de.bwl.bwfla.emil.utils.BuildContainerUtil;
-import de.bwl.bwfla.emucomp.api.*;
-import de.bwl.bwfla.imagebuilder.api.ImageContentDescription;
-import de.bwl.bwfla.imagebuilder.api.ImageDescription;
-import de.bwl.bwfla.imagebuilder.client.ImageBuilderClient;
-import org.apache.jena.atlas.logging.Log;
-import org.apache.tamaya.ConfigurationProvider;
+import de.bwl.bwfla.emucomp.api.ContainerConfiguration;
+import de.bwl.bwfla.emucomp.api.OciContainerConfiguration;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.Duration;
 import java.util.Optional;
 
 public class BuildContainerImageTask extends BlockingTask<Object> {
@@ -51,31 +39,32 @@ public class BuildContainerImageTask extends BlockingTask<Object> {
 
             String digest = (null == request.getDigest()) ?
                     getDigest(request.getUrlString(), request.getTag()) : request.getDigest();
+            request.setDigest(digest);
 
+            log.info("Checking digest: " + digest);
 
-            Log.info("BuildContainerImageTask", "Checking digest: " + digest);
-
-            Optional<ContainerConfiguration> existingContainer = archive.api()
+            final var fetchContainers = archive.api()
                     .v2()
                     .containers()
-                    .fetch()
-                    .stream()
-                    .filter(e -> e.getDigest() != null)
-                    .filter(e -> !e.isDeleted())
-                    .filter(e -> e.getDigest().equals(digest)).findFirst();
+                    .fetch();
 
-            //.forEach(e -> System.out.println("ContainerID:" + e.getId() + " Digest:" + e.getDigest()));
+            try (fetchContainers) {
+                Optional<ContainerConfiguration> existingContainer = fetchContainers
+                        .stream()
+                        .filter(e -> e.getDigest() != null)
+                        .filter(e -> !e.isDeleted())
+                        .filter(e -> e.getDigest().equals(digest)).findFirst();
 
+                //.forEach(e -> System.out.println("ContainerID:" + e.getId() + " Digest:" + e.getDigest()));
 
-            if (existingContainer.isPresent()) {
-                OciContainerConfiguration _config = (OciContainerConfiguration) existingContainer.get();
-                log.severe(" Container with digest " + digest + " already found, returning environment id: " + _config.getId());
+                if (existingContainer.isPresent()) {
+                    OciContainerConfiguration _config = (OciContainerConfiguration) existingContainer.get();
+                    log.info(" Container with digest " + digest + " already found, returning environment id: " + _config.getId());
 
-                return new ComponentResponse(_config.getId());
+                    return new ComponentResponse(_config.getId());
+                }
             }
-
         }
-
 
         try {
             return BuildContainerUtil.build(request);
@@ -85,7 +74,7 @@ public class BuildContainerImageTask extends BlockingTask<Object> {
         }
     }
 
-    private String getDigest(String imageRef, String tag) throws BWFLAException {
+    private String getDigest(String imageName, String tag) throws BWFLAException {
 
         if (tag == null)
             tag = "latest";
@@ -94,7 +83,7 @@ public class BuildContainerImageTask extends BlockingTask<Object> {
         runner.setLogger(log);
         runner.setCommand("crane");
         runner.addArgument("digest");
-        runner.addArgument(imageRef + ":" + tag);
+        runner.addArgument(imageName + ":" + tag);
         try {
             final DeprecatedProcessRunner.Result result = runner.executeWithResult(true)
                     .orElse(null);
