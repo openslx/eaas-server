ab9e6cc5b330f3839d8135c9fc5b41972d0950ef
Oleg Stobbe <oleg@openslx.com>
2022-03-24 03:30:04 +0100

    Add helpers for resolving component's images

---
+++ src/eaas/components/api/src/main/java/com/openslx/eaas/resolver/DataResolver.java
@@ -0,0 +1,63 @@
+/*
+ * This file is part of the Emulation-as-a-Service framework.
+ *
+ * The Emulation-as-a-Service framework is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * The Emulation-as-a-Service framework is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Emulation-as-a-Software framework.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.openslx.eaas.resolver;
+
+import de.bwl.bwfla.common.services.security.UserContext;
+import org.apache.tamaya.ConfigurationProvider;
+
+
+public class DataResolver
+{
+	protected final String endpoint;
+
+	public DataResolver(String endpoint)
+	{
+		if (endpoint == null || endpoint.isEmpty())
+			throw new IllegalArgumentException();
+
+		if (endpoint.endsWith("/"))
+			endpoint = endpoint.substring(0, endpoint.length() - 1);
+
+		this.endpoint = endpoint;
+	}
+
+	protected String resolve(UserContext userctx, String... subpaths)
+	{
+		final var location = new StringBuilder(4 * 1024)
+				.append(endpoint);
+
+		if (userctx != null && userctx.getToken() != null) {
+			location.append("/t/")
+					.append(userctx.getToken());
+		}
+
+		for (final var subpath : subpaths) {
+			location.append("/")
+					.append(subpath);
+		}
+
+		return location.toString();
+	}
+
+	public static String getDefaultEndpoint()
+	{
+		return ConfigurationProvider.getConfiguration()
+				.get("emucomp.resolver");
+	}
+}
